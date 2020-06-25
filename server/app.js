c49d216bc498358aa322ce4d3a12438266107b51
Sung Paik <sung.paik@codeimmersives.com>
2020-06-25 16:11:41 -0400

    initial commit

---
+++ server/app.js
@@ -0,0 +1,70 @@
+const createError = require("http-errors");
+const express = require("express");
+const compression = require("compression");
+const app = express();
+const socket_io = require("socket.io");
+const io = socket_io();
+app.io = io;
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const indexRouter = require("./routes/index")(io);
+const usersRouter = require("./routes/users");
+require("dotenv").config();
+
+mongoose
+  .connect(process.env.MONGO_DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("DB connection success!"))
+  .catch((e) => console.log(e));
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(cors({ origin: ["http://localhost:3000"] }));
+app.use(compression());
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
+io.on("connection", (socket) => {
+  io.clients((error, clients) => {
+    if (error) throw error;
+    console.log("clients", clients);
+  });
+
+  socket.on("position", (position) => {
+    socket.broadcast.emit("otherPositions", position);
+    console.log("position", position);
+  });
+});
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+module.exports = app;
---
+++ server/routes/index.js
@@ -0,0 +1,16 @@
+module.exports = function (io) {
+  const app = require("express");
+  const router = app.Router();
+
+  io.on("connection", function (socket) {
+    console.log(
+      `A socket connection to the server has been made: ${socket.id}`
+    );
+
+    socket.on("disconnect", () => {
+      console.log(`Connection ${socket.id} has left the building`);
+    });
+  });
+
+  return router;
+};
