2157fecc6ef0d64d1120eb4a5598f379a607be22
NikolayK <nikolay.kim@codeimmersive.com>
2020-07-07 13:00:42 -0400

    fixes active marker and sidebar css

--- client/src/components/redux/actions/activeMarkerActions.js
+++ client/src/components/redux/actions/activeMarkerActions.js
@@ -1,7 +1,6 @@
 import { SET_ACTIVE_MARKER } from '../constants/activeMarkerConstants'
 
 export const setActiveMarker = (markerInfo) => (dispatch) =>{
-   console.log(markerInfo)
    dispatch({
       type: SET_ACTIVE_MARKER,
       payload: markerInfo
